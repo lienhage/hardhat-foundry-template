37e11d30bc3f564edff4abea650fdc1fc205dd77
lienhage <lienhage@gmail.com>
2022-11-10 15:53:25 +0800

    initial commit

---
+++ README.md
@@ -0,0 +1,18 @@
+# Sample Hardhat-Foundry Project
+
+This project demonstrates a basic Hardhat develop suite with foundry testing framework build in. It comes with a sample contract, test cases for both hardhat and foundry, and a script that deploys that contract.
+
+Try running some of the following tasks:
+
+```shell
+npx hardhat help
+```
+npx hardhat test
+REPORT_GAS=true npx hardhat test
+or
+forge test
+```
+npx hardhat node
+npx hardhat run scripts/deploy.ts
+
+```
---
+++ hardhat.config.ts
@@ -0,0 +1,47 @@
+import { HardhatUserConfig } from "hardhat/config";
+import "@nomicfoundation/hardhat-toolbox";
+import "@nomiclabs/hardhat-solhint";
+import "dotenv/config";
+import "hardhat-gas-reporter";
+
+const DEPLOYER_PRIVATE_KEY =
+  process.env.DEPLOYER_PRIVATE_KEY ||
+  "0000000000000000000000000000000000000000000000000000000000000000";
+
+const ETHERSCAN_API_KEY = 
+  process.env.ETHERSCAN_API_KEY ||
+  "0000000000000000000000000000000000000000000000000000000000000000";
+
+const config: HardhatUserConfig = {
+  solidity: {
+    version: "0.8.9",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200,
+      }
+    }
+  },
+  networks: {
+    hardhat: {
+      forking: {
+        url: `https://eth-goerli.g.alchemy.com/v2/${process.env.ALCHEMY_MAINNET_API_KEY}`,
+        blockNumber:  15608656
+      }
+    },
+    mainnet: {
+      url: `https://eth-mainnet.alchemyapi.io/v2/${process.env.ALCHEMY_MAINNET_API_KEY}`,
+      accounts: [`0x${DEPLOYER_PRIVATE_KEY}`],
+    },
+    goerli: {
+      url: `https://eth-goerli.g.alchemy.com/v2/${process.env.ALCHEMY_GOERLI_API_KEY}`,
+      chainId:  5,
+      accounts: [`0x${DEPLOYER_PRIVATE_KEY}`]
+    },
+  },
+  etherscan: {
+    apiKey: `${ETHERSCAN_API_KEY}`
+  }
+};
+
+export default config;
